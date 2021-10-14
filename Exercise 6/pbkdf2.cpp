60f4e832ba2ec23c03349be8f593430d1533cde8
Odin Kvarving <odinkva@stud.ntnu.no>
2021-10-14 02:06:05 +0200

    Implemented pbkdf2-algorithm, still need to add function for brute-forcing password

--- Exercise 6/pbkdf2.cpp
+++ Exercise 6/pbkdf2.cpp
@@ -3,25 +3,34 @@
 #include <openssl/evp.h>
 #include <openssl/sha.h>
 #include <sstream>
-#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+enum { KEYLENGTH = 20, ITERATION = 2048 }; 
 
 /// Return hex string from bytes in input string.
-std::string hex(const std::string &input) {
-  std::stringstream hex_stream;
-  hex_stream << std::hex << std::internal << std::setfill('0');
+string hex(const string &input) {
+  stringstream hex_stream;
+  hex_stream << hex << internal << setfill('0');
   for (auto &byte : input)
-    hex_stream << std::setw(2) << (int)(unsigned char)byte;
+    hex_stream << setw(2) << (int)(unsigned char)byte;
   return hex_stream.str();
 }
 
-/// Return the SHA-512 (512-bit) hash from input.
-std::string sha512(const std::string &input) {
-  std::string hash;
-  hash.resize(512 / 8);
-  SHA512((const unsigned char *)&input[0], input.size(), (unsigned char *)&hash[0]);
+/// Return the SHA-1 (160-bit) hash from input.
+string pbkdf2(const string &input) {
+  string hash;
+  hash.resize(160 / 8);
+  unsigned char salt[] = "Saltet til Ola";
+
+  PKCS5_PBKDF2_HMAC_SHA1((char*)&input[0], -1, salt, sizeof(salt), ITERATION, KEYLENGTH, (unsigned char*)&hash[0]);
+
   return hash;
 }
 
 int main() {
-  std::cout << hex(sha512("Password123")) << std::endl;
+  cout << hex(pbkdf2("Passord987")) << endl;
 }
